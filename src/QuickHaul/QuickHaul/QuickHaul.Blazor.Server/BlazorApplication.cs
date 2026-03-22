c1bfc5148227c553433336d687f12ea1a71257fc
David Leonard <ghitadavidleonard@yahoo.com>
2026-03-22 10:45:16 +0200

    Add QuickHaul solution with XAF Blazor Server and Module projects

---
+++ src/QuickHaul/QuickHaul/QuickHaul.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Updating;
+using Microsoft.EntityFrameworkCore;
+using QuickHaul.Module.BusinessObjects;
+
+namespace QuickHaul.Blazor.Server
+{
+    public class QuickHaulBlazorApplication : BlazorApplication
+    {
+        public QuickHaulBlazorApplication()
+        {
+            ApplicationName = "QuickHaul";
+            CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            DatabaseVersionMismatch += QuickHaulBlazorApplication_DatabaseVersionMismatch;
+        }
+        protected override void OnSetupStarted()
+        {
+            base.OnSetupStarted();
+
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        }
+        void QuickHaulBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e)
+        {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else
+            {
+                string message = "The application cannot connect to the specified database, " +
+                    "because the database doesn't exist, its version is older " +
+                    "than that of the application or its schema does not match " +
+                    "the ORM data model structure. To avoid this error, use one " +
+                    "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if (e.CompatibilityError != null && e.CompatibilityError.Exception != null)
+                {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+    }
+}
---
+++ src/QuickHaul/QuickHaul/QuickHaul.Module/BusinessObjects/QuickHaulDbContext.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.ExpressApp.EFCore.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace QuickHaul.Module.BusinessObjects
+{
+    [TypesInfoInitializer(typeof(DbContextTypesInfoInitializer<QuickHaulEFCoreDbContext>))]
+    public class QuickHaulEFCoreDbContext : DbContext
+    {
+        public QuickHaulEFCoreDbContext(DbContextOptions<QuickHaulEFCoreDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+        public DbSet<ModelDifference> ModelDifferences { get; set; }
+        public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+        public DbSet<PermissionPolicyRole> Roles { get; set; }
+        public DbSet<QuickHaul.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+        public DbSet<QuickHaul.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginsInfo { get; set; }
+        public DbSet<DashboardData> DashboardData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.UseDeferredDeletion(this);
+            modelBuilder.UseOptimisticLock();
+            modelBuilder.SetOneToManyAssociationDeleteBehavior(DeleteBehavior.SetNull, DeleteBehavior.Cascade);
+            modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+            modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+            modelBuilder.Entity<QuickHaul.Module.BusinessObjects.ApplicationUserLoginInfo>(b =>
+            {
+                b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+            });
+            modelBuilder.Entity<ModelDifference>()
+                .HasMany(t => t.Aspects)
+                .WithOne(t => t.Owner)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
