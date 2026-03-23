c8fa65b6320d420c686303554a129e7cf507d7e2
David Leonard <ghitadavidleonard@yahoo.com>
2026-03-24 00:05:17 +0200

    Implement status change tracking and audit logging for DeliveryOrder

--- src/QuickHaul/QuickHaul/QuickHaul.Module/BusinessObjects/DeliveryEvent.cs
+++ src/QuickHaul/QuickHaul/QuickHaul.Module/BusinessObjects/DeliveryEvent.cs
@@ -1,25 +1,38 @@
-﻿using DevExpress.Persistent.BaseImpl.EF;
+﻿using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl.EF;
 using DevExpress.Persistent.Validation;
 using QuickHaul.Module.BusinessObjects.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace QuickHaul.Module.BusinessObjects
 {
-    public class DeliveryEvent: BaseObject
+    public class DeliveryEvent : BaseObject
     {
         [RuleRequiredField]
+        [ModelDefault("AllowEdit", "False")]
         public virtual DeliveryOrder DeliveryOrder { get; set; }
 
+        [ModelDefault("AllowEdit", "False")]
         public virtual DateTime Timestamp { get; set; }
 
+        [ModelDefault("AllowEdit", "False")]
         public virtual DeliveryOrderStatus? FromStatus { get; set; }
 
+        [ModelDefault("AllowEdit", "False")]
         public virtual DeliveryOrderStatus ToStatus { get; set; }
 
         [StringLength(100)]
+        [ModelDefault("AllowEdit", "False")]
         public virtual string ChangedBy { get; set; }
 
         [StringLength(500)]
         public virtual string Remarks { get; set; }
+
+        public override void OnSaving()
+        {
+            base.OnSaving();
+            if (Timestamp == default)
+                Timestamp = DateTime.UtcNow;
+        }
     }
 }
--- src/QuickHaul/QuickHaul/QuickHaul.Module/Controllers/DeliveryOrderController.cs
+++ src/QuickHaul/QuickHaul/QuickHaul.Module/Controllers/DeliveryOrderController.cs
@@ -1,4 +1,6 @@
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 using QuickHaul.Module.BusinessObjects;
 using QuickHaul.Module.BusinessObjects.Enums;
 using System.ComponentModel;
@@ -7,43 +9,66 @@ namespace QuickHaul.Module.Controllers
 {
     public class DeliveryOrderController : ObjectViewController<DetailView, DeliveryOrder>
     {
+        private DeliveryOrderStatus _previousStatus;
+
         protected override void OnActivated()
         {
             base.OnActivated();
+            _previousStatus = ViewCurrentObject.Status;
             ((INotifyPropertyChanged)ViewCurrentObject).PropertyChanged += Order_PropertyChanged;
             if (ObjectSpace.IsNewObject(ViewCurrentObject))
                 ObjectSpace.Committing += ObjectSpace_Committing;
         }
 
-        private void Order_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private void Order_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName != nameof(DeliveryOrder.Status)) return;
             var order = (DeliveryOrder)sender;
+
             if (order.Status == DeliveryOrderStatus.InTransit && order.ActualPickupDate == null)
                 order.ActualPickupDate = DateTime.Now;
             else if (order.Status == DeliveryOrderStatus.Delivered && order.ActualDeliveryDate == null)
                 order.ActualDeliveryDate = DateTime.Now;
+
+            var evt = ObjectSpace.CreateObject<DeliveryEvent>();
+            evt.DeliveryOrder = order;
+            evt.Timestamp = DateTime.UtcNow;
+            evt.FromStatus = _previousStatus;
+            evt.ToStatus = order.Status;
+            evt.ChangedBy = GetCurrentUserName();
+
+            _previousStatus = order.Status;
         }
 
         private void ObjectSpace_Committing(object sender, EventArgs e)
         {
+            ObjectSpace.Committing -= ObjectSpace_Committing;
+
             var order = ViewCurrentObject;
-            if (!string.IsNullOrEmpty(order.OrderNumber))
-                return;
-
-            string dateKey = DateTime.Today.ToString("yyyyMMdd");
-
-            var sequence = ObjectSpace.FirstOrDefault<OrderSequence>(s => s.DateKey == dateKey);
-            if (sequence == null)
+            if (string.IsNullOrEmpty(order.OrderNumber))
             {
-                sequence = ObjectSpace.CreateObject<OrderSequence>();
-                sequence.DateKey = dateKey;
-                sequence.LastSequence = 0;
+                string dateKey = DateTime.Today.ToString("yyyyMMdd");
+                var sequence = ObjectSpace.FirstOrDefault<OrderSequence>(s => s.DateKey == dateKey);
+                if (sequence == null)
+                {
+                    sequence = ObjectSpace.CreateObject<OrderSequence>();
+                    sequence.DateKey = dateKey;
+                    sequence.LastSequence = 0;
+                }
+                sequence.LastSequence++;
+                order.OrderNumber = $"DLV-{dateKey}-{sequence.LastSequence:D4}";
             }
 
-            sequence.LastSequence++;
-            order.OrderNumber = $"DLV-{dateKey}-{sequence.LastSequence:D4}";
+            var creationEvent = ObjectSpace.CreateObject<DeliveryEvent>();
+            creationEvent.DeliveryOrder = order;
+            creationEvent.Timestamp = DateTime.UtcNow;
+            creationEvent.FromStatus = null;
+            creationEvent.ToStatus = DeliveryOrderStatus.Created;
+            creationEvent.ChangedBy = GetCurrentUserName();
         }
+
+        private string GetCurrentUserName() =>
+            (SecuritySystem.CurrentUser as PermissionPolicyUser)?.UserName ?? "System";
 
         protected override void OnDeactivated()
         {
